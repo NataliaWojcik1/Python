9ea990ce83d4d9bb280b09ba7df347c6c53f7c7e
NataliaWojcik1 <natalia.malgorzata.wojcik@gmail.com>
2021-03-13 13:48:16 +0100

    Zadania domowe

---
+++ 02/Zadania_domowe/if_zad8.py
@@ -0,0 +1,13 @@
+# Sortowanie. Trzy dowolne liczby podane przez użytkownika zapisz do trzech zmiennych.
+# Znajdź największą liczbę. Wyświetl liczby od największej do najmniejszej.
+
+digit_1 = int(input('Podaj pierwsza liczbę: '))
+digit_2 = int(input('Podaj druga liczbę: '))
+digit_3 = int(input('Podaj trzecia liczbę: '))
+
+if digit_1 > digit_2 > digit_3:
+    print(digit_1, digit_2, digit_3)
+elif digit_3 > digit_2 > digit_1
+    print()
+elif digit_2 > digit_1 > digit_3
+    print()
