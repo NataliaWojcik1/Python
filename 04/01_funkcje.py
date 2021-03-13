ee002862df7cfae2642c5d1b6896ae3eee0a9adb
NataliaWojcik1 <natalia.malgorzata.wojcik@gmail.com>
2021-03-13 13:51:56 +0100

    Funkcje

---
+++ 04/01_funkcje.py
@@ -0,0 +1,32 @@
+def ask_about_mood():
+    question = 'How are you?'
+    print(question)
+
+
+def show(user_response):
+    print('^^^', user_response, '^^^')
+
+
+def  multiply_sentence(counter):
+    sentence = " I love Python!"
+    return sentence * counter
+
+def  multiply(quote, counter):
+    return (quote + '!') * counter
+
+# main part of the code
+# print('*********')
+# ask_about_mood()
+#
+# response = input('--> answer here: ')
+# show(response)
+#
+# exclamation = multiply_sentence(5)
+# print(exclamation)
+# print('-----END----')
+quote1 = multiply("It's not a bug!", 2)
+
+print(quote1)
+
+quote2=multiply("dupa",  3)
+print(quote2)
